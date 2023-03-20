d820d248d05117783dba9cd83ede29765b4e9944
darioelao <darioelao@gmail.com>
2023-03-20 14:33:39 -0400

    Added file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import NavBar from './components/NavBar';
+import Projects from './components/Projects';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+     <Header />
+     <Hero />
+     <NavBar />
+     <About />
+     <Projects />
+     <Contact />
     </div>
   );
 }
