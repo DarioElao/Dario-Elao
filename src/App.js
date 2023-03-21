4a1d6c4971c4b1b8e6e9ee0c780dd731fa4e9783
darioelao <darioelao@gmail.com>
2023-03-20 21:48:12 -0400

    Added tech compoment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
 import Projects from './components/Projects';
-
+import Tech from './components/Tech';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
      <Hero />
      <NavBar />
      <About />
+     <Tech />
      <Projects />
      <Contact />
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+  
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#d9d9d9',
+        accent: '#232323',
+      },
+      backgroundImage: {
+        site: "url('./assets/logo1.png')",
+        about: "url('./assets/logo2.png')",
+        tech: "url('./assets/logo3.png')",
+      },
+    },
+  },
+  plugins: [],
+};
