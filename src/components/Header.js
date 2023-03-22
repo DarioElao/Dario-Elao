6f8193a25068bdd1d88a00c7e46a30f400ab0130
darioelao <darioelao@gmail.com>
2023-03-22 00:56:45 -0400

    added logo to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Logo from '../assets/logo5.png'
 
 const Header = () => {
     return (
-    <div>Header</div>
+    <header className='fixed top  w-full overflow-hidden z-50'>
+      <div className='container mx-auto m-2'>
+        <div>
+          <a href='home'>
+            <img src={Logo} alt='logo' />
+          </a>
+        </div>
+      </div>
+    </header>
     );
   };
 
