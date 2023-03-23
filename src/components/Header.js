750ebe02e3897eb7980ae93bba734ab533204426
darioelao <darioelao@gmail.com>
2023-03-23 00:23:27 -0400

    Added hero component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Logo from '../assets/logo5.png'
+import Logo from '../assets/logo.svg'
 
 const Header = () => {
     return (
-    <header className='fixed top  w-full overflow-hidden z-50'>
-      <div className='container mx-auto m-2'>
-        <div>
-          <a href='home'>
+    <header >
+      {/* maintains logo at the top of the page at all times */}
+      {/* <div className='fixed top  w-full overflow-hidden z-50'></div> */}
+      <div className='container mx-auto my-3 w-full overflow-hidden z-50'>
+        <div className='w-[60px] lg:w-[75px]'>
+          <a href='#'>
             <img src={Logo} alt='logo' />
           </a>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,57 @@
 import React from 'react';
 
+import Image from '../assets/Untitled.png';
+import {BsLinkedin, BsGithub, BsInstagram} from 'react-icons/bs';
+import { TypeAnimation } from 'react-type-animation';
+import {motion} from 'framer-motion'
+import { fadeIn } from '../variants';
+
 const Hero = () => {
     return (
-    <div className='section' id='home'>Hero</div>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+      <div className='flex-1 text-center lg:text-left'>
+        <motion.h1 variants={fadeIn('up', 0.3)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[80px]'>
+          Dario <span>Elao</span>
+          </motion.h1>
+        <motion.div variants={fadeIn('up', 0.4)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 text-[30px] lg:text-[45px] font-semibold uppercase leading-[1]'>
+          <span className='mr-2'>I am a</span>
+          <TypeAnimation sequence={[
+            'Web Developer',
+            2000,
+          ]}
+          speed={5}
+          className='text-[#FDCA00]'
+          wrapper='span'
+          repeat={Infinity}/>
+         
+        </motion.div> 
+        <motion.p variants={fadeIn('up', 0.5)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-8 max-w-lg mx-auto lg:mx-0'>
+             Introduction paragraph
+          </motion.p>
+          <motion.div variants={fadeIn('up', 0.6)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            <button className='bg-[#D9D9D9] hover:bg-[#A6A6A6] text-[#232323] hover:text-[#FDCA00] font-bold py-1 px-4 rounded'>Contact</button>
+            <a href='#' className=''>My Portfolio</a>
+          </motion.div>
+          <motion.div variants={fadeIn('up', 0.7)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <a href='#'>
+              <BsLinkedin />
+            </a>
+            <a href='#'>
+              <BsGithub />
+            </a>
+            <a href='#'>
+              <BsInstagram />
+            </a>
+          </motion.div>
+      </div>
+      <motion.div variants={fadeIn('down', 0.5)}  initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mx-auto hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+        <img src={Image}/>
+      </motion.div>
+    </div>
+    </div>
+    </section>
     );
   };
 
